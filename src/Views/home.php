1eaad2ec3ad33f9c69e98ef9ff0cc64701a424a2
jan-brand <jan.brand@vdb.schule>
2026-04-10 23:24:53 +0200

    add Export Button

--- src/Views/home.php
+++ src/Views/home.php
@@ -13,6 +13,7 @@ $title = 'Trackly – Zeiterfassung für Vereine';
             <div class="l-cluster">
                 <a class="c-btn c-btn--primary" href="/login">Jetzt anmelden</a>
                 <a class="c-btn c-btn--secondary" href="/time-entries">Meine Zeiteinträge</a>
+                <a class="c-btn c-btn--secondary" href="/export">Exportieren</a>
             </div>
         </div>
     </div>
